df3f9b51cf9be0b94809f02242ab5a70e0ae8ee1
Artem <fb2kindle@gmail.com>
2022-09-13 19:30:03 +0300

    save

---
+++ Sklep/minimog-4437.kxcdn.com/wp-content/cache/autoptimize/1/js/autoptimize_single_104d1ecd9e3dc180d503da6ea33a7f5f.js
@@ -0,0 +1,12 @@
+(function($) {
+    'use strict';
+    var $body = $('body');
+    var MinimogGridDataHandler = function($scope, $) {
+        var $element = $scope.find('.minimog-grid-wrapper');
+        $element.MinimogGridLayout().MinimogGridQuery();
+    };
+    $(window).on('elementor/frontend/init', function() {
+        elementorFrontend.hooks.addAction('frontend/element_ready/tm-blog.default', MinimogGridDataHandler);
+        elementorFrontend.hooks.addAction('frontend/element_ready/tm-product.default', MinimogGridDataHandler);
+    });
+})(jQuery);
